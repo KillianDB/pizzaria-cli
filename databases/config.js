c29fce19c79bbb04b54a9b5c39a00026ce002f4a
KillianDB <killiandomingues57@gmail.com>
2023-03-29 11:25:05 -0300

    configurando sequelize e trazendo banco de dados

---
+++ databases/config.js
@@ -0,0 +1,24 @@
+{
+  "development": {
+    "username": "KillianDB",
+    "password": "vaiplaneta",
+    "database": "pizzaria_cli",
+    "host": "127.0.0.1",
+    "dialect": "mysql",
+    "port":"3000"
+  },
+  "test": {
+    "username": "root",
+    "password": null,
+    "database": "database_test",
+    "host": "127.0.0.1",
+    "dialect": "mysql"
+  },
+  "production": {
+    "username": "root",
+    "password": null,
+    "database": "database_production",
+    "host": "127.0.0.1",
+    "dialect": "mysql"
+  }
+}
