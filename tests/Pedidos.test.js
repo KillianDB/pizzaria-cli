1c2b8c7dc2946ed23942511abb12aa351d0de41e
KillianDB <killiandomingues57@gmail.com>
2023-04-14 09:16:20 -0300

    adicionando função async

--- tests/Pedidos.test.js
+++ tests/Pedidos.test.js
@@ -1,7 +1,7 @@
 const {Pedidos, sequelize} = require('../databases/models');
 
 async function teste(){
-    const pedidos = await Pedidos.findAll();
+    const pedidos = await Pedidos.findAll({include:'pizzas'});
     console.log(pedidos.map(p=>p.toJSON()));
     sequelize.close();
 }
